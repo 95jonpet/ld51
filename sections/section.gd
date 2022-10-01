9f295e5fd994d9e2ff7f3a454cee11785e8d641e
Peter Jonsson <4572231+95jonpet@users.noreply.github.com>
2022-10-02 00:12:39 +0200

    Generate levels

---
+++ sections/section.gd
@@ -0,0 +1,8 @@
+class_name Section
+extends Node2D
+
+const GRID_SIZE = 16
+const GRID_WIDTH := 10
+const GRID_HEIGHT := 8
+const WIDTH := GRID_WIDTH * GRID_SIZE
+const HEIGHT := GRID_HEIGHT * GRID_SIZE
